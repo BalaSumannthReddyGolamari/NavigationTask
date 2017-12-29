522dcfbe5fd6a9dbaf36250900433ffce366cbf7
BalaSumannthReddyGolamari <34880804+BalaSumannthReddyGolamari@users.noreply.github.com>
2017-12-29 16:12:00 +0530

    Add project files.

---
+++ NavigateButton/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NavigateButton.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult FirstValue(string id)
+        {
+            TempData["Value"]=id;
+            TempData.Keep("Value");
+            return View();
+        }
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
